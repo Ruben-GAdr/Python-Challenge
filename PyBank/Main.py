93d9c0283882daf9b6b201349f52a0913378967f
Ruben-GAdr <ruben.garciadr@gmail.com>
2024-07-17 02:41:48 -0600

    Challenge updated

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,54 +1,67 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
+# Import libraries
 import os
 import csv
-import pandas as pd
 
+# Set path of CSV file
 Budget_csv = os.path.join('..', "Pybank", "Resources", "budget_data.csv")
 
-profits_losses = []
+# Declare variables
+total_p = 0
+total_profit = 0
+prev_profit = None
+changes = []
+g_increase = {"date": "", "amount": 0}
+g_decrease = {"date": "", "amount": 0}
 
+# Open csv file
 with open(Budget_csv, mode='r', encoding='utf-8') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    header = next(csvreader)
-    df = pd.read_csv(Budget_csv) #Convert csv in dataframe
-    Total_P = len(df) # Get number of lines in dataframe (number of months)
-    for row in csvreader:
-        profits_losses.append(int(row[1])) #Create a list of changes between months
+    csv_header = next(csvreader)
+    data=list(csvreader) #Load data 
+    total_p = len(data) #Count rows in data
+    # loop through the dataset rows
+    for row in data:
+        date = row[0]
+        profit = int(row[1])
+        total_profit += profit # Sumamos total profit/losses
+        if prev_profit is not None: #Calculate and append changes between periods
+            change = profit - prev_profit
+            changes.append(change)
+            # Calculate max increase/decrease
+            if change > g_increase["amount"]:
+                g_increase = {"date": date, "amount": change}
+            if change < g_decrease["amount"]:
+                g_decrease = {"date": date, "amount": change}
+        
+        prev_profit = profit # set previous profit
 
-Total_Amount = "${:.0f}".format(sum(profits_losses)) # Get the total of Profit-Losses in database
-monthly_chg = [profits_losses[i] - profits_losses[i - 1] for i in range(1, len(profits_losses))] # Get the list of Profit-Losses changes in database
-avg_chg = sum(monthly_chg) / len(monthly_chg) # Get the mean of Profit-Losses in database
+avg_chg = round(sum(changes) / len(changes),2) if changes else 0 # Calculate average of changes
 
-max_change = max(monthly_chg) #Get max and min changes between periods in the database  
-min_change = min(monthly_chg)
+#Put result lines in a list
+results = [] 
+results.append("Financial Analysis") 
+results.append("") 
+results.append("--------------------------------------------")
+results.append("") 
+results.append(f"Total Months: {total_p}")
+results.append("") 
+results.append(f"Total: ${total_profit}")
+results.append("") 
+results.append(f"Average Change:  ${avg_chg}")
+results.append("") 
+results.append(f"Greatest Increase in Profits: {g_increase['date']} (${g_increase['amount']})")
+results.append("") 
+results.append(f"Greatest Decrease in Profits: {g_decrease['date']} (${g_decrease['amount']})")
 
-max_chg_idx = monthly_chg.index(max_change) + 1 #Get max and min changes index in the changes (P&L) list
-min_chg_idx = monthly_chg.index(min_change) + 1
-
-max_chg_period = df.loc[max_chg_idx, 'Date'] #Get period name of max/min changes
-min_chg_period = df.loc[min_chg_idx, 'Date']
-
-# Print resutls
-print("Financial Analysis")    
-print("--------------------------------------------")
-print("Total months: ", Total_P)
-print("Total: ", Total_Amount)
-print("Average Change: ", "${:.2f}".format(avg_chg))
-print(f'Greatest Increase in Profits: {max_chg_period} (${max_change})')
-print(f'Greatest Decrease in Profits: {min_chg_period} (${min_change})')
+#Print lines in terminal
+for line in results: #Print Results
+    print(line)
 
 #Write results to a txt file
-with open("Financial Analysis.txt", "w") as file:
-    file.write("Financial Analysis\n")
-    file.write("....................................................................................\n")
-    file.write("Total months: " + str(Total_P) + "\n")
-    file.write("Total: " + Total_Amount + "\n")
-    file.write("Average change: " + "${:.2f}".format(avg_chg) + "\n")
-    file.write(f"Greatest Increase in Profits:   {max_chg_period}  (${max_change})\n" )
-    file.write(f"Greatest Decrease in Profits:   {min_chg_period}  (${min_change})\n" )
-
+output_file_path = os.path.join("..","Pybank","Analysis",'Financial Analysis.txt') 
+with open(output_file_path, "w") as file:
+    for line in results:
+        file.write(line + '\n')
